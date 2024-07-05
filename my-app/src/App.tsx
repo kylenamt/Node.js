4a5e9c693f090f83e1c6ecadd2eb4763212dc89f
THIEU NAM KHANH <khanh.thieu@aalto.fi>
2024-07-05 14:42:42 +0700

    Experimenting with HTML and CSS

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -7,6 +7,25 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className ="Login-Box">
+          <div className = 'Login-logo'>
+            <area shape='rect' alt = '?' id = 'decoration-rectangle' ></area>
+            <h1 className = 'Login-title'> CRUD OPERATION</h1>
+          </div>
+          <h1 className = 'Sign-In'> SIGN IN</h1>
+          <h2 className = 'Sign-in-msg'> Enter your credentials to access your account</h2>
+          <div className = 'Email-Input'> 
+            <label >First name</label>
+            <input type="text" id="fname" name="fname"></input>
+          </div>
+          <div className = 'Password-Input'> 
+            <label >Password</label>
+            <input type="text" id="fname" name="fname"></input>
+          </div>
+          <button> Sign-in</button>
+          <div> 
+            <label> Forgot your password?</label>
+            <u>Reset Password</u>
+          </div>
         </div>
       </header>
     </div>
